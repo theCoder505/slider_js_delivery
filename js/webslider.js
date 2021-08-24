5b6a99d3e11dc66be2c17f34f6e89e106d499810
A.H.K. EMAD <68850204+theCoder505@users.noreply.github.com>
2021-08-25 00:10:35 +0600

    Add files via upload

---
+++ js/webslider.js
@@ -0,0 +1,32 @@
+var stackedCard = new stackedCards({
+    selector: '.stacked-cards',
+    layout: "fanOut",
+    transformOrigin: "bottom",
+});
+stackedCard.init();
+
+
+$('body').on('click', '#card-middle', function(e) {
+    document.getElementById("card-left").style.cssText = "transform-origin: center bottom; transform: translate(-100%, 0%) scale(0.8) rotate(-10deg); z-index: 11;"
+    document.getElementById("card-right").style.cssText = "transform-origin: center bottom;transform: translate(0%, 0%) scale(0.8) rotate(10deg);z-index: 9;"
+});
+
+
+setInterval(function() {
+
+    if (document.getElementsByClassName("jsCards")[0].classList.contains("runInterval")) {
+        document.getElementsByClassName("jsCards")[1].classList.add("runInterval");
+        document.getElementsByClassName("jsCards")[0].classList.remove("runInterval");
+        document.querySelector(".runInterval").click();
+    } else if (document.getElementsByClassName("jsCards")[1].classList.contains("runInterval")) {
+        document.getElementsByClassName("jsCards")[2].classList.add("runInterval");
+        document.getElementsByClassName("jsCards")[1].classList.remove("runInterval");
+        document.querySelector(".runInterval").click();
+    } else if (document.getElementsByClassName("jsCards")[2].classList.contains("runInterval")) {
+        document.getElementsByClassName("jsCards")[0].classList.add("runInterval");
+        document.getElementsByClassName("jsCards")[2].classList.remove("runInterval");
+        document.querySelector(".runInterval").click();
+    }
+
+
+}, 6000);
